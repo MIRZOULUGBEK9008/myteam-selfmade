7bb76fbdac12870b7786a0c7b73650d0fb280b52
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-11-07 20:05:57 +0500

    Update performance

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const elsPlus = document.querySelectorAll('.js-plus'),
-elsFront = document.querySelectorAll('.js-front');
-
-
+const elsPlus = document.querySelectorAll(".js-plus"),
+  elsFront = document.querySelectorAll(".js-front");
 elsPlus.forEach(function (plus, index) {
-  plus.addEventListener('click', function () {
-    elsFront[index].classList.toggle('team-members__info-front-wrapper--active');
-    plus.classList.toggle('plus--active');
+  plus.addEventListener("click", function () {
+    elsFront[index].classList.toggle(
+      "team-members__info-front-wrapper--active"
+    );
+    plus.classList.toggle("plus--active");
   });
 });
